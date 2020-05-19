cf42d6d47808a5cd464b7fa1a44887079a4ef634
Nico Korthout <nicokorthout@gmail.com>
2020-05-19 21:52:46 +0200

    refactor to upgrade to python 3

--- immo.py
+++ immo.py
@@ -21,7 +21,7 @@ while True:
         with open('href.json') as data_file:    
                 data = json.load(data_file)
         data=list(set([i[u'href'] for i in data]))
-        print data
+        print(data)
         ini=raw_input("No href.json file found. Sending messages to all offers found above?(y/n)\n")
         if ini.lower() == "y":
             data_old = []
@@ -32,7 +32,7 @@ while True:
             data_old=list(set([i[u'href'] for i in data_old]))
     #with open('href.json', 'w') as data_file:    
     #    json.dump(data,data_file)
-    #print data
+    #print(data)
     #black list
     if os.path.isfile('blacklist.json'):
         with open('blacklist.json') as blacklist:
@@ -40,21 +40,21 @@ while True:
         blacklist = list(set([i[u'href'] for i in blacklist]))
     else:
         blacklist = []
-    print "Blacklist: ", blacklist
+    print("Blacklist: ", blacklist)
 
     #with open('href_old.json', 'w') as data_old_file:    
     #    json.dump(data_old,data_old_file)
-    #print data_old
+    #print(data_old)
     diff_id=list(set(data)-set(data_old)-set(blacklist))
-    #print diff_id
+    #print(diff_id)
     text_file = open("sent_request.dat", "a")
     text_file1 = open("diff.dat", "a")
     if len(diff_id) != 0:
-        print len(diff_id), "new offers found"
-        print "New offers id:", diff_id
-        print "Time: ", datetime.now()
+        print(len(diff_id), "new offers found")
+        print("New offers id: ", diff_id)
+        print("Time: ", datetime.now())
         for new in diff_id:
-            print "Sending message to: ", new
+            print("Sending message to: ", new)
             submit.submit_app(new)
             text_file.write("ID: %s \n" % new)
             text_file.write(str(datetime.now())+'\n')
@@ -62,6 +62,6 @@ while True:
         text_file.close()
         text_file1.close()
     else:
-        print "No new offers."
-        print "Time: ", datetime.now()
+        print("No new offers.")
+        print("Time: ", datetime.now())
     time.sleep(60)
